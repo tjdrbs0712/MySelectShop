6ab38dcb890bebb0f34576eb1f9ca8247be28767
SeongKyun <yooss135@naver.com>
2024-05-24 11:07:01 +0900

    Scheduler 기능 구현

--- src/main/java/com/sparta/myselectshop/Service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/Service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -38,7 +39,6 @@ public class ProductService {
 
         product.update(requestDto);
 
-
         return new ProductResponseDto(product);
     }
 
@@ -49,13 +49,16 @@ public class ProductService {
         for (Product product : productList) {
             responseDtoList.add(new ProductResponseDto(product));
         }
-        
+
         return responseDtoList;
     }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 상품은 존재하지 않습니다.")
+        );
+
+        product.updateByItemDto(itemDto);
+    }
 }
-
-
-
-
-
-
---
+++ src/main/java/com/sparta/myselectshop/scheduler/Scheduler.java
@@ -0,0 +1,51 @@
+package com.sparta.myselectshop.scheduler;
+
+import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dto.ItemDto;
+import com.sparta.myselectshop.naver.service.NaverApiService;
+import com.sparta.myselectshop.repository.ProductRepository;
+import com.sparta.myselectshop.Service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j(topic = "Scheduler")
+@Component
+@RequiredArgsConstructor
+public class Scheduler {
+
+    private final NaverApiService naverApiService;
+    private final ProductService productService;
+    private final ProductRepository productRepository;
+
+    // 초, 분, 시, 일, 월, 주 순서
+    @Scheduled(cron = "0 0 1 * * *") // 매일 새벽 1시  //cron은 특정한 시간에 실행하도록 설정하는 방법
+    public void updatePrice() throws InterruptedException {
+        log.info("가격 업데이트 실행");
+        List<Product> productList = productRepository.findAll();
+        for (Product product : productList) {
+            // 1초에 한 상품 씩 조회합니다 (NAVER 제한)
+            TimeUnit.SECONDS.sleep(1); //1초 대기
+
+            // i 번째 관심 상품의 제목으로 검색을 실행합니다.
+            String title = product.getTitle();
+            List<ItemDto> itemDtoList = naverApiService.searchItems(title);
+
+            if (!itemDtoList.isEmpty()) {
+                ItemDto itemDto = itemDtoList.get(0);
+                // i 번째 관심 상품 정보를 업데이트합니다.
+                Long id = product.getId();
+                try {
+                    productService.updateBySearch(id, itemDto);
+                } catch (Exception e) {
+                    log.error(id + " : " + e.getMessage());
+                }
+            }
+        }
+    }
+
+}
